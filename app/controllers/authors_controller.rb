88c7537d819fe500a051520310627c8549bfba47
Mahati Gollamudi <github email address>
2016-08-20 23:26:06 -0400

    add validations and edited create method

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+
+
   def show
     @author = Author.find(params[:id])
   end
@@ -6,10 +8,16 @@ class AuthorsController < ApplicationController
   def new
   end
 
-  def create
-    @author = Author.create!(author_params)
 
-    redirect_to author_path(@author)
+
+  def create
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
